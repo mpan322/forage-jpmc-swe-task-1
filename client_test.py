5998bab91a1b93278f76c929aabfdd3851d3882a
mpan <mp322@st-andrews.ac.uk>
2024-08-10 12:44:22 +0100

    add unit tests for 'getRatio' and 'getDataPoint'

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,21 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected = [
+      ("ABC", 120.48, 121.2, (120.48 + 121.2) / 2),
+      ("DEF", 117.87, 121.68, (117.87 + 121.68) / 2)
+    ]
+    for quote, expected in zip(quotes, expected):
+      dataPoint = getDataPoint(quote)
+      self.assertEqual(dataPoint, expected)
+
+  def test_getRatio_b0(self):
+    result = getRatio(1.7, 0)
+    self.assertIsNone(result)
+
+  def test_getRatio_a0(self):
+    result = getRatio(0, 1.7)
+    self.assertEqual(result, 0)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +30,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    expected = [
+      ("ABC", 120.48, 119.2, (120.48 + 119.2) / 2),
+      ("DEF", 117.87, 121.68, (117.87 + 121.68) / 2)
+    ]
+    for quote, expected in zip(quotes, expected):
+      dataPoint = getDataPoint(quote)
+      self.assertEqual(dataPoint, expected)
 
   """ ------------ Add more unit tests ------------ """
 
